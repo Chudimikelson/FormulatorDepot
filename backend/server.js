ad66a0904b9e2f85712342d238eacdad425629ef
mike oselle <osellezino@outlook.com>
2021-08-08 21:11:12 +0100

    fresh commit

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ if (process.env.NODE_ENV === 'production') {
   )
 } else {
   app.get('/', (req, res) => {
-    res.send('API is running....')
+    res.sendFile(path.resolve(__dirname, '../frontend', 'src', 'index.js'))
   })
 }
 
